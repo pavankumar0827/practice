757cda1c269eb51319c742e0650efe7591a8c772
pavankumar0827 <yenumulashankarpavankumar@gmail.com>
2025-11-29 21:08:41 +0530

    Create HelloController.java

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import com.example.demo.controller.HelloController;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloControllerTest {
+
+    @Test
+    public void testSayHello() {
+        HelloController controller = new HelloController();
+        String result = controller.sayHello();
+        assertEquals("Hello from Jenkins + Maven project!", result);
+    }
+}
