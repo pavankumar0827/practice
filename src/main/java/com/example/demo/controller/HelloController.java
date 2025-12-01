546c0881f0f1b77398f56e994edbde2310307a33
pavankumar0827 <yenumulashankarpavankumar@gmail.com>
2025-12-01 08:59:36 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello from Jenkins + Maven project!";
+        return "Hello from Jenkin + Maven project!";
     }
 }
